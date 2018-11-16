f0bd8e5b032ad1f566101087d89872caa80a3afd
Fabián Rodríguez Rodríguez <fabianrguezrguez@gmail.com>
2018-11-16 19:12:13 +0000

    Creada nueva estructura

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,54 @@
 <template>
-  <div>
-    <h1>¡VueJS 2.0!</h1>
-    <img src="./assets/logo.png" alt="VueJS Logo">
+  <div class='container'>
+    <div class='header'>
+      <h1>Fabián Rodríguez Rodríguez</h1>
+    </div>
+    <div class='contact'>
+      <html-contact></html-contact>
+    </div>
+    <div class='content'>
+      <img src="./assets/logo.png" alt="Foto de Fabián">
+    </div>
   </div>
 </template>
 
 <style>
-  :root {
-    text-align: center;
-  }
-  
-  h1 {
-    font-family: Montserrat, Impact, sans-serif;
-    font-size: 72px;
-  }
+.header {
+  grid-area: header;
+}
+
+.contact {
+  grid-area: contact;
+}
+
+.content {
+  grid-area: content;
+}
+
+.footer {
+  grid-area: footer;
+}
+
+.container {
+  display: grid;
+  grid-template-areas:
+    "header header header header header header"
+    "contact content content content content content";
+
+  text-align: center;
+}
+
+h1 {
+  font-family: Montserrat, Impact, sans-serif;
+  font-size: 48px;
+}
 </style>
+
+<script>
+import Contact from "./components/Contact";
+export default {
+  components: {
+    "html-contact": Contact
+  }
+};
+</script>
---
+++ src/components/Contact.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="ContactHTML">
+        <div class="ContactTitle">
+            <div>Contacto</div>
+        </div>
+        <div class="ContactOptions">
+
+        </div>
+    </div>
+</template>
